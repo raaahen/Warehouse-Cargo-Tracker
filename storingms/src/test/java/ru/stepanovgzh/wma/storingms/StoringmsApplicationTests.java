3d0fc7c3e4e305df71f99592c36d310f426edc92
sgzh1 <ya.superkesa@gmail.com>
2024-05-10 20:08:19 +0300

    added storing microservice

---
+++ storingms/src/test/java/ru/stepanovgzh/wma/storingms/StoringmsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stepanovgzh.wma.storingms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoringmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

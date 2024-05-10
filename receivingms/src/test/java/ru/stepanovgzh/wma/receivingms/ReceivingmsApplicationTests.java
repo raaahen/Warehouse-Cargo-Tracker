5c187fbfcb35cc539676ec3e3352b77bec54af3f
sgzh1 <ya.superkesa@gmail.com>
2024-05-10 19:06:52 +0300

    added receiving microservice

---
+++ receivingms/src/test/java/ru/stepanovgzh/wma/receivingms/ReceivingmsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stepanovgzh.wma.receivingms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceivingmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

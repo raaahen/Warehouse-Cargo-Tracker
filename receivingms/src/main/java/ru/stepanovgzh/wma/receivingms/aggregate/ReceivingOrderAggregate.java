7ad06f111eac59b604fa4260b90b2661470da79b
sgzh1 <ya.superkesa@gmail.com>
2024-05-17 03:39:23 +0300

    added fields for receivindOrderAggregate, added classes for:  main entities, values, commands

--- receivingms/src/main/java/ru/stepanovgzh/wma/receivingms/aggregate/ReceivingOrderAggregate.java
+++ receivingms/src/main/java/ru/stepanovgzh/wma/receivingms/aggregate/ReceivingOrderAggregate.java
@@ -1,11 +1,17 @@
 package ru.stepanovgzh.wma.receivingms.aggregate;
 
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import org.axonframework.modelling.command.TargetAggregateIdentifier;
 import org.axonframework.spring.stereotype.Aggregate;
 
 import lombok.NoArgsConstructor;
+import ru.stepanovgzh.wma.receivingms.data.model.ReceivingOrderDetail;
+import ru.stepanovgzh.wma.receivingms.data.model.Supplier;
+import ru.stepanovgzh.wma.receivingms.data.model.Transporter;
+import ru.stepanovgzh.wma.receivingms.data.value.ReceivingStatus;
 
 @Aggregate
 @NoArgsConstructor
@@ -13,5 +19,9 @@ public class ReceivingOrderAggregate
 {
     @TargetAggregateIdentifier
     UUID id;
-
+    Supplier supplier;
+    Transporter transporter;
+    Date date;
+    ReceivingStatus status;
+    List<ReceivingOrderDetail> details;
 }
---
+++ receivingms/src/main/java/ru/stepanovgzh/wma/receivingms/cqrs/command/ReceiveCargoCommand.java
@@ -0,0 +1,5 @@
+package ru.stepanovgzh.wma.receivingms.cqrs.command;
+
+public class ReceiveCargoCommand {
+    
+}

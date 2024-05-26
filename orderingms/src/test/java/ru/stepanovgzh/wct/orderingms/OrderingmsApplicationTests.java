ff86512472364664370343fb4837543701054a36
sgzh1 <ya.superkesa@gmail.com>
2024-05-26 16:57:19 +0300

    added ordering microservice with all maven dependencies and configs

---
+++ orderingms/src/test/java/ru/stepanovgzh/wct/orderingms/OrderingmsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stepanovgzh.wct.orderingms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
